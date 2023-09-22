b89fbe6fd579f0123dc019634a2e18f9251532d6
AryanNesti <aryannesti60@gmail.com>
2023-09-22 18:14:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/codepathmail/Email.kt
@@ -0,0 +1,7 @@
+package com.example.codepathmail
+
+import android.graphics.drawable.Drawable
+
+class Email (val sender: String, val title: String, val summary: String, val date: String, val icon: Int) {
+
+}
